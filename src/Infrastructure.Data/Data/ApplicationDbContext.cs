e6213da3eb36ae7aa9a8756fda55d7d95305a3f1
Claudinei José Santos <claudineibitta@gmail.com>
2023-03-19 23:51:35 -0300

    refactor|feat: organizacao da camada de infraestrutura

---
+++ src/Infrastructure.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DomainModels.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<Pessoa> Pessoas { get; set; }
+}
