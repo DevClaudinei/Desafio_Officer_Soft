c9b9acedbfdc79a3c283dd1ba7375f73d3adde9b
Claudinei José Santos <claudineibitta@gmail.com>
2023-03-19 23:52:55 -0300

    feat: criacao de validacoes e regras para validacao de campos

---
+++ src/AppServices/Validations/StringExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AppServices.Validations
+{
+    public static class StringExtensions
+    {
+        public static bool DocumentoEhValido(this string document)
+        {
+            if (document.Length != 11) return false;
+
+            var firstDigitChecker = 0;
+            for (int i = 0; i < document.Length - 2; i++)
+            {
+                firstDigitChecker += document.ConverteParaInteiro(i) * (10 - i);
+            }
+            firstDigitChecker = firstDigitChecker * 10 % 11;
+            if (firstDigitChecker is 10) firstDigitChecker = 0;
+
+            var secondDigitChecker = 0;
+            for (int i = 0; i < document.Length - 1; i++)
+            {
+                secondDigitChecker += document.ConverteParaInteiro(i) * (11 - i);
+            }
+            secondDigitChecker = secondDigitChecker * 10 % 11;
+            if (secondDigitChecker is 10) secondDigitChecker = 0;
+
+            return firstDigitChecker.Equals(document.ConverteParaInteiro(^2)) && secondDigitChecker.Equals(document.ConverteParaInteiro(^1));
+        }
+
+        public static int ConverteParaInteiro(this string cpf, Index index)
+        {
+            var indexValue = index.IsFromEnd
+                ? cpf.Length - index.Value
+                : index.Value;
+
+            return (int)char.GetNumericValue(cpf, indexValue);
+        }
+
+        public static bool ContemEspacoEmBranco(this string fields)
+            => fields.Split(" ").Any(x => x == string.Empty);
+
+        public static bool ExisteAlgumSimboloOuCaracterEspecial(this string value)
+            => value.Replace(" ", string.Empty).Any(x => !char.IsLetter(x));
+
+        public static bool TemPeloMenosDoisCaracteresParaCadaPalavra(this string fields)
+            => !fields.Split(" ").Where(x => x.Length < 2).Any();
+
+        public static bool EhUmCepValido(this string cep)
+        {
+            var expression = "([0-9]{5}-[0-9]{3})";
+            return Regex.Match(cep, expression).Success;
+        }
+    }
+}
---
+++ src/AppServices/Validations/ValidadorCriaCadastro.cs
@@ -0,0 +1,58 @@
+﻿using Application.Models.DTOs;
+using FluentValidation;
+
+namespace AppServices.Validations
+{
+    public class ValidadorCriaCadastro : AbstractValidator<CriaCadastro>
+    {
+        public ValidadorCriaCadastro()
+        {
+            RuleFor(x => x.Cpf)
+                .NotEmpty()
+                .NotNull()
+                .Must(x => x.DocumentoEhValido());
+
+            RuleFor(x => x.Nome)
+                .NotEmpty()
+                .NotNull()
+                .Must(x => x.Split(" ").Length > 1)
+                .WithMessage("FullName deve conter ao menos um sobrenome")
+                .Must(x => !x.ContemEspacoEmBranco())
+                .WithMessage("FullName não deve conter espaços em branco")
+                .Must(x => !x.ExisteAlgumSimboloOuCaracterEspecial())
+                .WithMessage("FullName não deve conter caracteres especiais")
+                .Must(x => x.TemPeloMenosDoisCaracteresParaCadaPalavra())
+                .WithMessage("FullName inválido. Nome e/ou sobrenome devem conter ao menos duas letras ou mais");
+
+            RuleFor(x => x.Cep)
+                .NotEmpty()
+                .NotNull()
+                .Must(x => x.EhUmCepValido())
+                .WithMessage("O campo PostalCode deve estar no formato XXXXX-XXX");
+
+            RuleFor(x => x.Numero)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Bairro)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Complemento)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Municipio)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Uf)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Rg)
+                .NotEmpty()
+                .NotNull();
+        }
+    }
+}
