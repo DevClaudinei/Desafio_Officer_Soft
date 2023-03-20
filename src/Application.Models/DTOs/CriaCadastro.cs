18ee8da1890e4549d004b25b67efb39b654b0da6
Claudinei José Santos <claudineibitta@gmail.com>
2023-03-19 23:49:58 -0300

    refactor|feat: instalacao de pacotes e criacao de dtos

---
+++ src/Application.Models/DTOs/CriaCadastro.cs
@@ -0,0 +1,43 @@
+﻿namespace Application.Models.DTOs
+{
+    public class CriaCadastro
+    {
+        public CriaCadastro() { }
+
+        public CriaCadastro(
+            string cpf,
+            string nome,
+            string cep,
+            string endereco,
+            int numero,
+            string bairro,
+            string complemento,
+            string municipio,
+            string uf,
+            string rg
+        )
+        {
+            Cpf = FormatCPF(cpf.Trim());
+            Nome = nome;
+            Cep = cep.Trim();
+            Endereco = endereco;
+            Numero = numero;
+            Bairro = bairro;
+            Complemento = complemento;
+            Municipio = municipio;
+            Uf = uf;
+            Rg = rg;
+        }
+
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public string Cep { get; set; }
+        public string Endereco { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Complemento { get; set; }
+        public string Municipio { get; set; }
+        public string Uf { get; set; }
+        public string Rg { get; set; }
+    }
+}
---
+++ src/Application.Models/DTOs/PessoaInfo.cs
@@ -0,0 +1,17 @@
+﻿using DomainModels.Interfaces;
+using System;
+
+namespace Application.Models.DTOs
+{
+    public class PessoaInfo : IIdentificavel
+    {
+        public PessoaInfo() { }
+
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+        public string Cep { get; set; }
+        public DateTime DataDeCriacao { get; set; }
+    }
+}
