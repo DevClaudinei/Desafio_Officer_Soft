34837239020b336b4430c7d741af209e585b0ca4
Claudinei José Santos <claudineibitta@gmail.com>
2023-03-19 23:50:29 -0300

    feat: criacao de perfil para mapeamento entre entidade de dominio e dtos

---
+++ src/AppServices/Profiles/PessoaProfile.cs
@@ -0,0 +1,22 @@
+﻿using Application.Models.DTOs;
+using AutoMapper;
+using DomainModels.Entities;
+
+namespace AppServices.Profiles
+{
+    public class PessoaProfile : Profile
+    {
+        public PessoaProfile() 
+        {
+            CreateMap<AtualizaCadastro, Pessoa>().ReverseMap();
+            CreateMap<CriaCadastro, Pessoa>();
+            CreateMap<Pessoa, RetornoPessoaCadastrada>();
+            CreateMap<Pessoa, PessoaInfo>()
+                .ForMember(x => x.Nome, opts => opts.MapFrom(x => x.Nome))
+                .ForMember(x => x.Cpf, opts => opts.MapFrom(x => x.Cpf))
+                .ForMember(x => x.Rg, opts => opts.MapFrom(x => x.Rg))
+                .ForMember(x => x.Cep, opts => opts.MapFrom(x => x.Cep))
+                .ForMember(x => x.DataDeCriacao, opts => opts.MapFrom(x => x.DataDeCriacao));
+        }
+    }
+}
