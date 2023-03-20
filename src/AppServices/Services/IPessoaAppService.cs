abef16f9ae233a09d0aee563a1f0e16c3ddb9565
Claudinei José Santos <claudineibitta@gmail.com>
2023-03-19 23:58:58 -0300

    feat|refactor: mudancas na camada de aplicacao

---
+++ src/AppServices/Services/IPessoaAppService.cs
@@ -0,0 +1,16 @@
+﻿using Application.Models.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AppServices.Services
+{
+    public interface IPessoaAppService
+    {
+        Task<long> CadastraPessoa(CriaCadastro criaCadastro);
+        Task<AtualizaCadastro> MostraPessoaPorId(long id);
+        Task<PessoaInfo> BuscaPessoaPorId(long id);
+        Task<List<PessoaInfo>> MostraTodosCadastrados();
+        void AtualizCadastro(long id, AtualizaCadastro atualizaCadastro);
+        void ExcluiPessoa(long id);
+    }
+}
