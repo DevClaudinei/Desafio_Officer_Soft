486164380f2e307e8d044dd5fc4b39cad81be365
Claudinei José Santos <claudineibitta@gmail.com>
2023-03-19 23:53:32 -0300

    refactor: remocao de models desnecessarias

--- src/CadastroPessoa/Models/ErrorViewModel.cs
+++ src/CadastroPessoa/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace CadastroPessoa.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
--- src/CadastroPessoa/Models/Usuario.cs
+++
@@ -1,9 +0,0 @@
-namespace CadastroPessoa.Models
-{
-    public class Usuario
-    {
-        public long Id {get; set;}
-        public string Email {get; set;}
-        public string Senha {get; set;}
-    }
-}
