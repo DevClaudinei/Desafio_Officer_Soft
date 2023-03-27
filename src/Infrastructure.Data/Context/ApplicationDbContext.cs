b5184ccf9911a284ab3b423dc818913f5b4cdc74
Claudinei José Santos <claudineibitta@gmail.com>
2023-03-27 20:16:13 -0300

    refactor: correcao das configuracoes do contexto do identity

--- src/Infrastructure.Data/Context/ApplicationDbContext.cs
+++ src/Infrastructure.Data/Context/ApplicationDbContext.cs
@@ -7,9 +7,4 @@ public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options) { }
-
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
-    }
 }
