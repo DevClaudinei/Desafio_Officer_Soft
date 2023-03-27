ebc169f98e52911bfc2eebb80d570b5915138ba2
Claudinei José Santos <claudineibitta@gmail.com>
2023-03-27 18:39:40 -0300

    feat|refactor: insercao de uma service base e mudancas na camada de servico

--- src/DomainServices/Services/Interfaces/IPessoaService.cs
+++ src/DomainServices/Services/Interfaces/IPessoaService.cs
@@ -7,9 +7,8 @@ namespace DomainServices.Services.Interfaces
     public interface IPessoaService
     {
         Task<long> CadastraPessoa(Pessoa pessoa);
-        Task<Pessoa> MostraPessoaPorId(long id);
-        Task<IEnumerable<Pessoa>> MostraTodosCadastrados();
-        void AtualizCadastro(Pessoa pessoa);
+        IEnumerable<Pessoa> MostraTodosCadastrados();
+        void AtualizCadastro(long id, Pessoa pessoa);
         void ExcluiPessoa(long id);
         Task<Pessoa> BuscaPessoaPorId(long id);
         Task<Pessoa> BuscaPessoaPeloNome(string name);
