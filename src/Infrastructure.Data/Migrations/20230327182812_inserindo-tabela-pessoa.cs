9022cb11d5787c1c574adc26f77073bdb279cf27
Claudinei José Santos <claudineibitta@gmail.com>
2023-03-27 18:49:16 -0300

    feat|refactor: criacao de map para pessoa e novas migrations

---
+++ src/Infrastructure.Data/Migrations/20230327182812_inserindo-tabela-pessoa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class inserindotabelapessoa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoa",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Cpf = table.Column<string>(type: "varchar(11)", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(150)", nullable: false),
+                    Cep = table.Column<string>(type: "varchar(9)", nullable: false),
+                    Endereco = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Numero = table.Column<int>(type: "int", nullable: false),
+                    Bairro = table.Column<string>(type: "varchar(60)", nullable: false),
+                    Complemento = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Municipio = table.Column<string>(type: "TEXT", nullable: true),
+                    Uf = table.Column<string>(type: "varchar(2)", nullable: false),
+                    Rg = table.Column<string>(type: "varchar(10)", nullable: false),
+                    DataDeCriacao = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoa", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoa");
+        }
+    }
+}
