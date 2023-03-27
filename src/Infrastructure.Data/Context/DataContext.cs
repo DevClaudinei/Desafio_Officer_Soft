dccdaf52ceb824cff3fabbcef226dab991f47b44
Claudinei José Santos <claudineibitta@gmail.com>
2023-03-27 18:47:33 -0300

    feat: criando contextos separados

---
+++ src/Infrastructure.Data/Context/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Infrastructure.Data.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            ChangeTracker.LazyLoadingEnabled = false;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.Load("Infrastructure.Data"));
+        }
+    }
+}
